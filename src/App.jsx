fca7858bd58c102c7f92fad2c8ec7153125d5b2e
ethanliu24 <liu.ethan2005@gmail.com>
2024-12-07 19:27:05 -0500

    set up react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Routes, Route } from "react-router-dom"
+import Home from "./components/Home/Home.jsx"
+
 function App() {
   return (
-    <>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   )
 }
 
