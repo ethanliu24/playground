b051bafdb62a4bbd940b73b3db113fb8758bded0
ethanliu24 <liu.ethan2005@gmail.com>
2024-12-10 15:04:34 -0500

    modify clip visual imports so vite can find them in build

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { Routes, Route } from "react-router-dom";
+import bgImg from "./bgImgs.js";
 import PageTransition from "./components/common/PageTransition.jsx";
 import Home from "./components/home/Home.jsx";
 
 function App() {
-  const pathToNature = "src/assets/nature/"; // In React, paths are relative to the public
   const routes = [
     {
       path: "/",
       element: <Home />,
       title: "Hey Hello",
       subtitle: "I got diabetes ahsdkfhjsa sdljkf asflkkasd h",
-      clipVisual: "forest.gif",
+      clipVisual: bgImg.forestGIF,
     }
   ];
 
@@ -25,7 +25,7 @@ function App() {
               page={data.element}
               title={data.title}
               subtitle={data.subtitle}
-              clipVisualPath={pathToNature + data.clipVisual}
+              clipVisualPath={data.clipVisual}
             />
           }
         />
---
+++ src/bgImgs.js
@@ -0,0 +1,14 @@
+// Import background images for text clipping so Vite can access them in build.
+import forestGIF from "./assets/nature/forest.gif";
+import forestIMG from "./assets/nature/forest.jpg";
+import sakuraGIF from "./assets/nature/sakura.gif";
+import sakuraIMG from "./assets/nature/sakura.jpg";
+
+const bgImg = {
+  forestGIF,
+  forestIMG,
+  sakuraGIF,
+  sakuraIMG,
+};
+
+export default bgImg;
