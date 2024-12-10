0710df8820f3227fbf6ab61469112e761e940e31
ethanliu24 <liu.ethan2005@gmail.com>
2024-12-10 00:14:17 -0500

    refactor routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import PageTransition from "./components/common/PageTransition.jsx";
 import Home from "./components/home/Home.jsx";
 
 function App() {
-  const location = useLocation();
+  const routes = [
+    {
+      path: "/", element: <Home />, title: "Hey", subtitle: "I got diabetes", clipVisual: "",
+    }
+  ];
 
   return (
-    <Routes location={location} key={location.pathname}>
-      <Route index element={<PageTransition page={<Home />} />} />
+    <Routes>
+      {routes.map((data) => {
+        <Route
+          key={data.path}
+          path={data.path}
+          element={<PageTransition page={data.element} />}
+          title={data.title}
+          subtitle={data.subtitle}
+          clipVisual={data.clipVisual}
+        />
+      })}
+      <Route path="/" element={<PageTransition page={<Home />} />} />
     </Routes>
   );
 }
