d12c297896927ad00e4f8894193aad0412c65cd1
ethanliu24 <liu.ethan2005@gmail.com>
2024-12-11 01:53:17 -0500

    add loading animation to loading text

--- src/components/common/Loading.jsx
+++ src/components/common/Loading.jsx
@@ -1,10 +1,43 @@
 import { useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
 
 export default function Loading() {
   const [waitMessage, setWaitMessage] = useState("");
 
-  const startTime = Date.now();
+  useGSAP(() => {
+    const tl = gsap.timeline({ repeat: -1 });
 
+    const loadingTextNodeList = document.querySelectorAll(".loading-text-elem");
+    const loader = [document.getElementById("loader")];
+    const loadingText = Array.from(loadingTextNodeList);
+
+    gsap.timeline({ repeat: -1 }).to(loader.at(0), { duration: 1, rotate: 360, ease: "none" }) // loader spin animation
+    tl.to(loadingText.concat(loader), {
+      opacity: 1,
+      stagger: 0.08,
+      duration: 0.7,
+      ease: "none",
+    }).to(loadingText, {
+      keyframes: {
+        "50%": { y: -20 },
+        "100%": { y: 0 },
+      },
+      stagger: 0.13,
+      duration: 0.7,
+      ease: "sine.inOut"
+    }).to(loadingText.concat(loader), {
+      y: -20,
+      opacity: 0,
+      stagger: 0.05,
+      duration: 5,
+      ease: "expo.out",
+    }, 6);
+  }, []);
+
+  const startTime = Date.now();
 
   return (
     <>
@@ -14,8 +47,9 @@ export default function Loading() {
       <div id="loading-text-container">
         <div id="loading-text">
           {("LOADING").split("").map((letter) => {
-            return <span className="loading-text-letter" key={letter}>{letter}</span>
+            return <div className="loading-text-elem" key={letter}>{letter}</div>
           })}
+          <div id="loader"></div>
         </div>
         <div id="wait-text">wait</div>
       </div>
