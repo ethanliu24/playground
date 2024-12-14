2d11a7547a76ae1b8619cb4898d841e71d742675
ethanliu24 <liu.ethan2005@gmail.com>
2024-12-14 02:34:50 -0500

    add botanica swell 2

--- src/utils/transitionAudio.js
+++ src/utils/transitionAudio.js
@@ -1,4 +1,5 @@
 import botanica_1 from "../assets/botanica_swells/botanica_1.wav";
+import botanica_2 from "../assets/botanica_swells/botanica_2.wav";
 
 function audioData(src, length, impactTime = 0.5) {
   return {
@@ -9,7 +10,9 @@ function audioData(src, length, impactTime = 0.5) {
 }
 
 const botanica1 = audioData(botanica_1, 6, 0.4);
+const botanica2 = audioData(botanica_2, 8, 0.4)
 
 export const bgAudio = {
   botanica1,
+  botanica2,
 }
