59f11c30e46c7ab98592f0731e6912a2a17f168c
ethanliu24 <liu.ethan2005@gmail.com>
2024-12-30 18:32:29 -0500

    add icons

---
+++ src/utils/icons.js
@@ -0,0 +1,14 @@
+import headphones from "../assets/icons/headphones.svg";
+import github from "../assets/icons/github.svg";
+import allowTransition from "../assets/icons/allow_transition.svg";
+import blockTransition from "../assets/icons/block_transition.svg";
+
+
+const icons = {
+  headphones,
+  github,
+  allowTransition,
+  blockTransition,
+};
+
+export { icons };
