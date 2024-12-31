be032c20832b5ae84b1670fa3eca202badd4c029
ethanliu24 <liu.ethan2005@gmail.com>
2024-12-30 20:09:02 -0500

    add tool tip style

--- src/components/general/NavBar.jsx
+++ src/components/general/NavBar.jsx
@@ -8,18 +8,24 @@ export default function NavBar() {
   const handleBlockTransition = () => {
     setBlockTransition(b => !b);
     localStorage.setItem("blockTransition", JSON.stringify(!blockTransition));
-    console.log(typeof JSON.parse(localStorage.getItem("blockTransition")))
   }
 
   return (
     <nav className="nav-bar">
       <div className="nav-home"><Link to="/">Home</Link></div>
       <div className="nav-right">
-        <img src={blockTransition ? icons.blockTransition : icons.allowTransition}
-          id="allow-transition-icon"
-          className="icon"
-          onClick={handleBlockTransition} />
-        <img src={icons.github} className="icon" />
+        <div className="tooltip-ref-obj">
+          <img src={blockTransition ? icons.blockTransition : icons.allowTransition}
+            id="allow-transition-icon"
+            className="icon"
+            onClick={handleBlockTransition} />
+          <span className="tooltip">Block transitions</span>
+        </div>
+
+        <div className="tooltip-ref-obj">
+          <img src={icons.github} className="icon" />
+          <span className="tooltip">See repo</span>
+        </div>
       </div>
     </nav>
   );
