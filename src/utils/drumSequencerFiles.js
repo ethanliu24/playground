7dbddec1acc60441747c0c66ae425101c6d31ba5
ethanliu24 <liu.ethan2005@gmail.com>
2025-01-05 02:23:50 -0500

    add drum sequencer samples

---
+++ src/utils/drumSequencerFiles.js
@@ -0,0 +1,35 @@
+import spinz808 from "../assets/drum_sequencer_sounds/808_spinz_C.mp3";
+import clap from "../assets/drum_sequencer_sounds/clap.mp3";
+import crash from "../assets/drum_sequencer_sounds/crash.mp3";
+import djScratch from "../assets/drum_sequencer_sounds/dj_scratch.mp3";
+import glassBell from "../assets/drum_sequencer_sounds/glass_bell.mp3";
+import hihatMain from "../assets/drum_sequencer_sounds/hihat_main.mp3";
+import hihatSoft from "../assets/drum_sequencer_sounds/hihat_soft.mp3";
+import hitB from "../assets/drum_sequencer_sounds/hit_B.mp3";
+import hitC from "../assets/drum_sequencer_sounds/hit_C.mp3";
+import kick from "../assets/drum_sequencer_sounds/kick.mp3";
+import openHat from "../assets/drum_sequencer_sounds/open_hat.mp3";
+import snare from "../assets/drum_sequencer_sounds/snare.mp3";
+import tambourine from "../assets/drum_sequencer_sounds/tamb.mp3";
+import triangle from "../assets/drum_sequencer_sounds/triangle.mp3";
+import woodblock from "../assets/drum_sequencer_sounds/woodblock.mp3";
+
+const samples = [
+  kick,
+  spinz808,
+  snare,
+  hitC,
+  hitB,
+  crash,
+  hihatMain,
+  hihatSoft,
+  openHat,
+  tambourine,
+  clap,
+  glassBell,
+  triangle,
+  woodblock,
+  djScratch,
+];
+
+export { samples };
