ffa66f67ccca73d6fc0392fbdc2fc970407d081a
ethanliu24 <liu.ethan2005@gmail.com>
2025-01-05 22:01:32 -0500

    modify ui

--- src/components/drum_sequencer/DrumSequencerSettings.jsx
+++ src/components/drum_sequencer/DrumSequencerSettings.jsx
@@ -14,18 +14,16 @@ export default function DrumSequencerSettings(props) {
     <div className="sequencer-settings-container">
       <img src={props.playing ? icons.pause : icons.play} id="sequencer-play-btn" className="icon" onClick={props.handlePlay} />
       <Knob initialAngle={30} maxAngle={330} minAngle={30} id="swing-knob" />
-      <div id="bpm-container" className="slider-container channel-rack-ui">
-        <input type="range" min="40" max="200" value={props.bpm} className="slider" onChange={updateBPM} ref={bpmSliderRef} />
-        <div id="bpm-value" className="slider-value">{`${props.bpm} bpm`}</div>
-      </div>
-      <select name="bars" id="bars-selector" className="dropdown-selector channel-rack-ui" value="4" onChange={() => {}}>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="4">4</option>
-        <option value="8">8</option>
-        <option value="16">16</option>
+      <select name="bars" id="bars-selector" className="dropdown-selector channel-rack-ui" onChange={() => {}}>
+        <option value="1">1 bar</option>
+        <option value="2">2 bars</option>
+        <option value="4">4 bars</option>
+        <option value="8">8 bars</option>
+        <option value="16">16 bars</option>
       </select>
-      <button id="clear-btn" className="channel-rack-ui" onClick={props.clearGrid}>Clear</button>
+      <button id="clear-btn" className="channel-rack-ui" onClick={props.clearGrid}>clear</button>
+      <input type="range" min="40" max="200" value={props.bpm} className="slider" onChange={updateBPM} ref={bpmSliderRef} />
+      <div id="bpm-value" className="channel-rack-ui slider-value">{`bpm: ${props.bpm}`}</div>
     </div>
   );
 }
