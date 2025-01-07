fe8dbd3cbb14efa02c5cd90a9c93df2217298d47
ethanliu24 <liu.ethan2005@gmail.com>
2025-01-06 23:06:37 -0500

    create hip hop pattern

---
+++ src/components/drum_sequencer/patternCreator.js
@@ -0,0 +1,92 @@
+/**
+ * To make a pattern, create a function and fill in valid data to construct a json file.
+ * Then, call that function. Refer to jsonParser.js on how to construct data.
+ */
+import { createTrack, createPattern } from "./jsonParser.js";
+import { sampleNames } from "../../utils/drumSequencerFiles.js";
+import * as C from "./constants.js";
+
+export function createHipHop() {
+  const tracks = [
+    createTrack(
+      sampleNames.kick, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      [
+       1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+       1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0,
+      ]
+    ),
+    createTrack(
+      sampleNames.spinz808, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      [
+       1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0,
+      ]
+    ),
+    createTrack(
+      sampleNames.snarePower, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      [
+       0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+       0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+      ]
+    ),
+    createTrack(
+      sampleNames.hitC, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      [
+       1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      ]
+    ),
+    createTrack(
+      sampleNames.crash, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      [
+       1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      ]
+    ),
+    createTrack(
+      sampleNames.hihatMain, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      [
+       0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0,
+       0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0,
+      ]
+    ),
+    createTrack(
+      sampleNames.hihatSoft, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      [
+       1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0,
+       1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0,
+      ]
+    ),
+    createTrack(
+      sampleNames.openHat, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      [
+       0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      ]
+    ),
+    createTrack(
+      sampleNames.glassBell, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      [
+       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+       0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0,
+      ]
+    ),
+    createTrack(
+      sampleNames.triangle, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      [
+       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0,
+      ]
+    ),
+    createTrack(
+      sampleNames.djScratch, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      [
+       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+      ]
+    ),
+  ];
+
+  createPattern("hip_hop", 0, 2, 97, tracks);
+}
+
