6884fdddfb9f7bd6608ac383cd9c1964610e9672
ethanliu24 <liu.ethan2005@gmail.com>
2025-01-07 01:53:28 -0500

    update hip hop preset

--- src/components/drum_sequencer/presets.js
+++ src/components/drum_sequencer/presets.js
@@ -2,91 +2,91 @@
  * To make a pattern, create a function and fill in valid data to construct a json file.
  * Then, call that function. Refer to jsonParser.js on how to construct data.
  */
-import { createTrack, createPreset } from "./jsonParser.js";
+import { createTrack, createPreset } from "./presetParser.js";
 import { sampleNames } from "../../utils/drumSequencerFiles.js";
 import * as C from "./constants.js";
 
 export function createHipHop() {
   const tracks = [
     createTrack(
-      sampleNames.kick, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      sampleNames.kick, false, C.CHANNEL_DEFAULT_VOL_PERCENTAGE, C.CHANNEL_DEFAULT_PAN_PERCENTAGE,
       [
        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0,
       ]
     ),
     createTrack(
-      sampleNames.spinz808, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      sampleNames.spinz808, false, C.CHANNEL_DEFAULT_VOL_PERCENTAGE, C.CHANNEL_DEFAULT_PAN_PERCENTAGE,
       [
        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0,
       ]
     ),
     createTrack(
-      sampleNames.snarePower, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      sampleNames.snarePower, false, C.CHANNEL_DEFAULT_VOL_PERCENTAGE, C.CHANNEL_DEFAULT_PAN_PERCENTAGE,
       [
        0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
        0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
       ]
     ),
     createTrack(
-      sampleNames.hitC, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
-      [
-       1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-       0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-      ]
-    ),
-    createTrack(
-      sampleNames.clap, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
-      [
-       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-       0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-      ]
-    ),
-    createTrack(
-      sampleNames.crash, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      sampleNames.hitC, false, C.CHANNEL_DEFAULT_VOL_PERCENTAGE, C.CHANNEL_DEFAULT_PAN_PERCENTAGE,
       [
        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
       ]
     ),
     createTrack(
-      sampleNames.hihatMain, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      sampleNames.clap, false, 1, C.CHANNEL_DEFAULT_PAN_PERCENTAGE,
+      [
+       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+       0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+      ]
+    ),
+    createTrack(
+      sampleNames.crash, false, C.CHANNEL_DEFAULT_VOL_PERCENTAGE, C.CHANNEL_DEFAULT_PAN_PERCENTAGE,
+      [
+       1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      ]
+    ),
+    createTrack(
+      sampleNames.hihatMain, false, C.CHANNEL_DEFAULT_VOL_PERCENTAGE, C.CHANNEL_DEFAULT_PAN_PERCENTAGE,
       [
        0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0,
        0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0,
       ]
     ),
     createTrack(
-      sampleNames.hihatSoft, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      sampleNames.hihatSoft, false, C.CHANNEL_DEFAULT_VOL_PERCENTAGE, C.CHANNEL_DEFAULT_PAN_PERCENTAGE,
       [
        1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0,
        1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0,
       ]
     ),
     createTrack(
-      sampleNames.openHat, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      sampleNames.openHat, false, C.CHANNEL_DEFAULT_VOL_PERCENTAGE, C.CHANNEL_DEFAULT_PAN_PERCENTAGE,
       [
        0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
       ]
     ),
     createTrack(
-      sampleNames.glassBell, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      sampleNames.glassBell, false, C.CHANNEL_DEFAULT_VOL_PERCENTAGE, C.CHANNEL_DEFAULT_PAN_PERCENTAGE,
       [
        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
        0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
       ]
     ),
     createTrack(
-      sampleNames.triangle, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      sampleNames.triangle, false, C.CHANNEL_DEFAULT_VOL_PERCENTAGE, C.CHANNEL_DEFAULT_PAN_PERCENTAGE,
       [
        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0,
       ]
     ),
     createTrack(
-      sampleNames.djScratch, false, C.CHANNEL_DEFAULT_VOL, C.CHANNEL_DEFAULT_PAN,
+      sampleNames.djScratch, false, C.CHANNEL_DEFAULT_VOL_PERCENTAGE, C.CHANNEL_DEFAULT_PAN_PERCENTAGE,
       [
        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
