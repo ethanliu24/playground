15323575e7eeae85c50b518e9c3804b83a5d7282
ethanliu24 <liu.ethan2005@gmail.com>
2025-01-07 01:48:09 -0500

    update knob rotation based on props initial angle

--- src/components/drum_sequencer/Knob.jsx
+++ src/components/drum_sequencer/Knob.jsx
@@ -6,27 +6,33 @@ import { ROTATION_STYLE_REGEX } from "./constants";
 gsap.registerPlugin(Draggable)
 
 export default function Knob(props) {
-  const [angle, setAngle] = useState(props.initialAngle); // in degrees
-
   const knobRef = useRef(null);
 
   useEffect(() => {
     const knob = knobRef.current;
-    knob.style.transform = `rotate(${angle}deg)`
+    updateAngle(props.initialAngle);
 
     Draggable.create(knob, {
       type: "rotation",
       onDrag: () => {
         const matchRotation = knob.style.transform.match(ROTATION_STYLE_REGEX);
         const rawAngle = matchRotation ? Math.floor(parseFloat(matchRotation[1])) : props.initialAngle;
-        const clampedAngle = Math.max(props.minAngle, Math.min(props.maxAngle, rawAngle));
-        knob.style.transform = `rotate(${clampedAngle}deg)`;
+        const clampedAngle = updateAngle(rawAngle);
         const anglePercentage = (clampedAngle - props.minAngle) / (props.maxAngle - props.minAngle);
         props.updateKnobFunction(anglePercentage);
-        setAngle(clampedAngle);
       }
     });
   }, []);
+
+  useEffect(() => {
+    updateAngle(props.initialAngle);
+  }, [props.initialAngle])
+
+  const updateAngle = (rawAngle) => {
+    const clampedAngle = Math.max(props.minAngle, Math.min(props.maxAngle, rawAngle));
+    knobRef.current.style.transform = `rotate(${clampedAngle}deg)`;
+    return clampedAngle;
+  }
 
   return (
     <div id={props.id ? props.id : ""} className={`knob ${props.className ? props.className : ""}`} ref={knobRef}></div>
